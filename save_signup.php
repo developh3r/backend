60e9406279b7de56af74918187ce586003c059dc
CoderClara <coderclaracoder@gmail.com>
2019-07-07 00:20:15 +0800

    Change save_signup.php method from JSON to POST

--- save_signup.php
+++ save_signup.php
@@ -4,13 +4,9 @@
        
 
     include('config.php');
-    $json = file_get_contents('php://input');
-        // Converts it into a PHP object
-        $data = json_decode($json);
-        
-        
+    
         //check if existing
-        $check=mysqli_query($con,"SELECT * from tbluser where email='".$data->email."'");
+        $check=mysqli_query($con,"SELECT * from tbluser where email='".$_POST['email']."'");
             $numrow=mysqli_num_rows($check);
             if($numrow<>0)
             {
@@ -38,14 +34,14 @@
                         $user_id=str_pad($id, 7, '0', STR_PAD_LEFT);
                     }
 
-                    $query=mysqli_query($con,"INSERT INTO tbluser (id,name,email,user_id,datetime) VALUES ('','".$data->name."','".$data->email."','".$useri_id."','".date('Y-m-d H:i:s')."')");
+                    $query=mysqli_query($con,"INSERT INTO tbluser (id,name,email,user_id,datetime) VALUES ('','".$_POST['name']."','".$_POST['email']."','".$useri_id."','".date('Y-m-d H:i:s')."')");
             
                     //get user id to restore in tbllogin
-                    $get_user=mysqli_query($con,"SELECT * FROM tbluser where email='".$data->email."'");
+                    $get_user=mysqli_query($con,"SELECT * FROM tbluser where email='".$_POST['email']."'");
                         $row_get=mysqli_fetch_assoc($get_user);
 
                     //save login to tbllogin
-                    $save_login=mysqli_query($con,"INSERT INTO tbllogin (id,email,password,user_id,datetime) values ('','".$row_get['email']."','".md5($data->password)."','".$row_get['user_id']."','".date('Y-m-d H:i:s')."')");
+                    $save_login=mysqli_query($con,"INSERT INTO tbllogin (id,email,password,user_id,datetime) values ('','".$row_get['email']."','".md5($_POST['password'])."','".$row_get['user_id']."','".date('Y-m-d H:i:s')."')");
             
                     //redirect to display and pass the data
                     header('Location:https://mirrorbackend.milkylorejo.com/save_signup_display.php?user_id='.$row_get['user_id'].'');
